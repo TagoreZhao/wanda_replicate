fd4ad74b42af5cbe0b44a41aeed71405a4458e69
TagoreZhao <zhaotagore@gmail.com>
2024-11-01 09:50:39 +0000

    update

--- test.sh
+++ test.sh
@@ -10,24 +10,26 @@
 # Set common variables
 model="meta-llama/Llama-2-7b-chat-hf"
 sparsity_ratio=0.5
-export MASTER_PORT=$((12000 + RANDOM % 1000))  # Assign a unique port
-export OMP_NUM_THREADS=1  # Limit CPU threads per process
+export MASTER_PORT=$((12000 + RANDOM % 1000))  # Unique port for each job
+export OMP_NUM_THREADS=1
 
-# Run the pruning script with MPI and torchrun
-echo "Running with MPI and wanda pruning method on a single node with 4 GPUs"
+# Run pruning with torchrun
+echo "Running wanda pruning with MPI on a single node with 4 GPUs"
 
 mpirun -np 4 torchrun \
-    --nproc_per_node=4 main.py \
+    --nproc_per_node=4 \
+    --rdzv_backend=c10d \
+    --rdzv_endpoint=localhost:${MASTER_PORT} \
+    main.py \
     --model $model \
     --prune_method "wanda" \
     --sparsity_ratio $sparsity_ratio \
     --sparsity_type "2:4" \
     --save "out/llama_7b/2-4/wanda/"
 
-# Wait for all background processes
 wait
-
 echo "Finished wanda pruning with MPI on a single node"
+
 
 # Explicitly exit
 exit 0
